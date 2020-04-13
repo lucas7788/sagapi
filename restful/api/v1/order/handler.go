1a6e3851254f665cd98436e6030f4819559c428d
lucas7788 <sishsh@163.com>
2020-04-13 20:07:38 +0800

    refactor code

--- restful/api/v1/order/handler.go
+++ restful/api/v1/order/handler.go
@@ -1,4 +1,4 @@
-package v1
+package order
 
 import "github.com/gin-gonic/gin"
 
--- restful/api/v1/v1_routes.go
+++ restful/api/v1/v1_routes.go
@@ -3,11 +3,11 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ontio/sagapi/restful/api/v1/nasa"
+	"github.com/ontio/sagapi/restful/api/v1/order"
 )
 
 func RoutesV1(parent *gin.RouterGroup) {
 	v1Route := parent.Group("/v1")
 	nasa.RoutesNasa(v1Route)
-	parent.POST("/takeOrder", TakeOrder)
-	parent.POST("/payOrder", PayOrder)
+	order.RoutesOrder(v1Route)
 }
