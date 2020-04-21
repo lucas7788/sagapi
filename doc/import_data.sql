335805fdcbd87837f59d76645b1e436d2561e7e3
lucas <sishsh@163.com>
2020-04-21 11:15:49 +0800

    add import (#13)

--- doc/import_data.sql
+++ doc/import_data.sql
@@ -14,10 +14,29 @@ INSERT INTO `tbl_category`(`id`, `name_zh`, `name_en`, `icon`, `state`) VALUES (
 INSERT INTO `tbl_category`(`id`, `name_zh`, `name_en`, `icon`, `state`) VALUES (14, '气象', 'Climate', 'http://a7b7e5478639411ea83090a4ed185dbd-1921228004.ap-southeast-1.elb.amazonaws.com/img/icons/Climate.svg', 1);
 INSERT INTO `tbl_category`(`id`, `name_zh`, `name_en`, `icon`, `state`) VALUES (15, '城市', 'Demography', 'http://a7b7e5478639411ea83090a4ed185dbd-1921228004.ap-southeast-1.elb.amazonaws.com/img/icons/Demography.svg', 1);
 INSERT INTO `tbl_category`(`id`, `name_zh`, `name_en`, `icon`, `state`) VALUES (16, '出行', 'Mobility', 'http://a7b7e5478639411ea83090a4ed185dbd-1921228004.ap-southeast-1.elb.amazonaws.com/img/icons/Mobility.svg', 1);
+
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (1, 'Agriculture', 1, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (2, 'Business', 2, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (3, 'General', 3, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (4, 'COVID-19', 4, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (5, 'Other', 5, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (6, 'Science', 6, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (7, 'Finance', 7, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (8, 'Entertainment', 8, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (9, 'Internet of Things', 9, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (10, 'Geography', 10, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (11, 'Medicine', 11, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (12, 'Lifestyle', 12, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (13, 'Social', 13, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (14, 'Climate', 14, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (15, 'Demography', 15, 1, '2020-03-13 10:16:22');
+INSERT INTO `tbl_tag`(`id`, `name`, `category_id`, `state`, `create_time`) VALUES (16, 'Mobility', 16, 1, '2020-03-13 10:16:22');
+
 insert into tbl_api_basic_info (Icon, Title, ApiProvider, ApiUrl, Price, ApiDesc,Specifications,Popularity,Delay,SuccessRate,InvokeFrequency) values ('http://a7b7e5478639411ea83090a4ed185dbd-1921228004.ap-southeast-1.elb.amazonaws.com/img/icons/Geography.svg','每日天文图片','https://api.nasa.gov/', "http://127.0.0.1:8080/api/v1/nasa/apod/:apikey", '', '获取每日天文信息',1,100,0,100,100);
 insert into tbl_api_basic_info (Icon, Title, ApiProvider, ApiUrl, Price, ApiDesc,Specifications,Popularity,Delay,SuccessRate,InvokeFrequency) values ('http://a7b7e5478639411ea83090a4ed185dbd-1921228004.ap-southeast-1.elb.amazonaws.com/img/icons/Geography.svg','近地小行星信息','https://api.nasa.gov/', "http://127.0.0.1:8080/api/v1/nasa/feed/:startdate/:enddate/:apikey", '', '近地小行星信息',1,100,0,100,100);
 insert into tbl_api_detail_info (ApiId, Mark, ResponseParam, ResponseExample, DataDesc, DataSource,ApplicationScenario) values (1,'mark','ResponseParam', '{"copyright":"Juan Filas","date":"2020-04-20","explanation":"To some, it looks like a giant chicken running across the sky. To others, it looks like a gaseous nebula where star formation takes place. Cataloged as IC 2944, the Running Chicken Nebula spans about 100 light years and lies about 6,000 light years away toward the constellation of the Centaur (Centaurus).  The featured image, shown in scientifically assigned colors, was captured recently in a 12-hour exposure. The star cluster Collinder 249 is visible embedded in the nebula''s glowing gas.  Although difficult to discern here, several dark molecular clouds with distinct shapes can be found inside the nebula.","hdurl":"https://apod.nasa.gov/apod/image/2004/IC2944_Filas_3320.jpg","media_type":"image","service_version":"v1","title":"IC 2944: The Running Chicken Nebula","url":"https://apod.nasa.gov/apod/image/2004/IC2944_Filas_960.jpg"}
 ','每日天文图片','nasa','');
 insert into tbl_api_detail_info (ApiId, Mark, ResponseParam, ResponseExample, DataDesc, DataSource,ApplicationScenario) values (2,'mark','ResponseParam','ResponseExample','近地小行星信息','nasa','');
-insert into tbl_api_tag
+insert into tbl_api_tag (api_id, tag_id,state) values (1,10,1);
+insert into tbl_api_tag (api_id, tag_id,state) values (2,10,1);
 
