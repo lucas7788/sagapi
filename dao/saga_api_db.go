cd1bda3df9299d4f180ac88e4b99e4293859674c
lucas7788 <sishsh@163.com>
2020-04-28 17:54:55 +0800

    clear code

--- dao/saga_api_db.go
+++ dao/saga_api_db.go
@@ -2,7 +2,6 @@ package dao
 
 import (
 	"context"
-	"database/sql"
 	"github.com/jmoiron/sqlx"
 	"github.com/ontio/sagapi/sagaconfig"
 	"time"
@@ -18,23 +17,6 @@ type SagaApiDB struct {
 }
 
 func NewSagaApiDB(dbConfig *sagaconfig.DBConfig) (*SagaApiDB, error) {
-	db, dberr := sql.Open("mysql",
-		dbConfig.ProjectDBUser+
-			":"+dbConfig.ProjectDBPassword+
-			"@tcp("+dbConfig.ProjectDBUrl+
-			")/"+dbConfig.ProjectDBName+
-			"?charset=utf8&parseTime=true")
-	if dberr != nil {
-		return nil, dberr
-	}
-	ctx, cf := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cf()
-	err := db.PingContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	//==============
 	dbx, dberr := sqlx.Open("mysql",
 		dbConfig.ProjectDBUser+
 			":"+dbConfig.ProjectDBPassword+
@@ -45,17 +27,17 @@ func NewSagaApiDB(dbConfig *sagaconfig.DBConfig) (*SagaApiDB, error) {
 		return nil, dberr
 	}
 
-	err = dbx.PingContext(ctx)
+	ctx, cf := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cf()
+
+	err := dbx.PingContext(ctx)
 	if err != nil {
 		return nil, err
 	}
-	//==============
-
 	return &SagaApiDB{
 		ApiDB:    NewApiDB(dbx),
 		OrderDB:  NewOrderDB(dbx),
 		QrCodeDB: NewQrCodeDB(dbx),
-		OtherDB:  NewOtherDB(db),
 	}, nil
 }
 
